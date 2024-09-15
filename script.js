d4a12451677806a40f34a773930b06f85caace6c
marwaneltary <marwaneltary@gmail.com>
2024-09-15 19:13:36 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+var colors=["blue","red","pink","purpile","yello","green"];
+       for(var d=0; d<colors.length; d=d+1){
+               console.log(colors[d]);
+  }
+
+for(var color of colors){
+         console.log(color)
+}
+
+var names=["ahmed","mohamed","marwan","hassan","mamdoh"]
+for(var name of names){
+         console.log(name)
+}
+
+ var subjects=["science","arabic","englich","social","math"]
+   for(var subject of subjects){
+           console.log(subject);
+ }
+var students=[
+
+{name:"odai",age:9},
+{name:"marwan",age:11}
+];
+
+ for(var i =0;i <students.length;i = i+1){
+
+ console.log(students[i].name);
+ console.log(students[i].age);
+ }
+
+
+ for (var student of students){
+ console.log(student.name);
+  console.log(student.age);
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
